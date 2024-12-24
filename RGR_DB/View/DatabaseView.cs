bb116e79db28ae1c86039700c7f52f2dc4cffdcd
AlinaKonovalenko <alina.konovalenko.ak@gmail.com>
2024-12-24 03:04:41 +0200

    Add files via upload

---
+++ RGR_DB/View/DatabaseView.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PostgreConsoleInteractorCS
+{
+    public class DatabaseView
+    {
+        DatabaseController controller;
+
+        public void Process()
+        {
+            string connection_string = "Server=localhost;Port=5432;User ID=postgres;Password=0000;Database=postgres;";
+            controller = new DatabaseController(connection_string);
+            while (true)
+            {
+                int action = controller.ShowActionList();
+                controller.Execute(action);
+            }
+        }
+    }
+}
